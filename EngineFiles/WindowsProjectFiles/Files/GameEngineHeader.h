fc9e2c407d80f2afa8afe1d81ce2f007e532b862
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-02-24 00:52:17 -0500

    Engines Getting Started

---
+++ EngineFiles/WindowsProjectFiles/Files/GameEngineHeader.h
@@ -0,0 +1,23 @@
+#include<Windows.h>
+#include<string>
+#include<iostream>
+#include<vector>
+#include<random>
+#include<map>
+#include<cstdlib>
+#include<Windows.h>
+#include<ctime>
+#include<cmath>
+#include<conio.h>
+#include<algorithm>
+#ifndef UNICODE
+#define UNICODE
+#endif 
+#define PI 3.14
+constexpr auto ABSOLUTECENTERX = 0;
+constexpr auto ABSOLUTECENTERY = 0;
+constexpr auto ABSOLUTECENTERZ = 0;
+
+struct veccoordinates {
+    double x, y, z;
+};
