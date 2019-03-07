ff948d05fa2cfab7d3957cf6e933f3f8cc0a7ddf
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-03-06 19:00:12 -0500

    reorgainzing

--- EngineFiles/Files/FBXFileParser.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameEngineHeader.h"
-#include "FBXFileParser.h"
-using namespace std;
-
-void FBXFileParser::fbxparser(string fname, vector<double>xyzcoords) {
-    
-}
--- EngineFiles/Files/OBJParser.cpp
+++
@@ -1,7 +0,0 @@
-#include "OBJParser.h"
-#include "Files/GameEngineHeader.h"
-using namespace std;
-
-void ObjFileParser::objparser(string fname, vector<double>xyzcoordinates) {
-    
-}
