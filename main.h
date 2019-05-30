9202904b8ceeb95c450b799589cd38708afd1f86
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-05-30 09:19:50 -0400

    Add files via upload

--- main.h
+++ main.h
@@ -2,16 +2,15 @@
 #include "core/String.h"
 namespace enginecore {
 
-    int width, height;
+    int width, height = 0;
 
     void initializeWindow(int w, int h, String title) {
-        const char* name = "bhellol";
         width = w;
         height = h;
         glutInitDisplayMode(GLUT_DEPTH | GLUT_RGBA | GLUT_DOUBLE);
         glEnable(GL_DEPTH_TEST);
         glutInitWindowSize(width, height);
+        glutCreateWindow(title.str);
         glutMainLoop();
-
     }
 }
