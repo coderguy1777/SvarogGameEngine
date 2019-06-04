41ad0b7708fdb2a1f76378e3d47e9ef651216cf0
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-06-04 13:36:46 -0400

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,2 @@
+g++ main.cpp glad/glad.c -o /home/jordan/Documents/PersonalProjects/SvarogGameEngine/builds/SvarogEngine -lglfw -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -I.
+exit 0
