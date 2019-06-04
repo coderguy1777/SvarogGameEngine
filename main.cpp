ec86129a8b2254646d8e1bee4a7c5b2d57cdc7a9
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-06-04 13:13:53 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,54 +1,21 @@
-#include "main/Color.h"
-#include "core/Vector.h"
-#include "EngineHeader.h"
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<iostream>
 #include "main.h"
-#include <vector>
+#include "physics/vec3.h"
+#include "main/Color.h"
+#include "main/HSV.h"
+#include "main/cmyk.h"
+#include "main/hsl.h"
+#include "main/htmlcolors.h"
+#include "core/math.h"
+#include "core/ArrayList.h"
 using namespace std;
 
-int fac = 0;
-Vector vec(0, 0, 0);
-void test(unsigned char key, int x, int y) {
-	Vector vec(0, 0, 0);
-	if (key == 'w') {
-		fac += 1;
-		glutPostRedisplay();
-	}
-
-	if (key == 's') {
-		fac -= sin(1);
-		glutPostRedisplay();
-	}
-}
-
-void init() {
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-1, 1, -1.5, 1.5, 1, 2000);
-}
-
-void renderScene(void) {
-	HSV newColor(165, 0.96, 0.65);
-	Rgb newColor2(newColor.HSVtoRGB());
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-	glMatrixMode(GL_MODELVIEW);
-	glClearColor(newColor2.redval, newColor2.greenval, newColor2.blueval, 0.0f);
-	init();
-	glutSwapBuffers();
-}
-
-
-int main(int argc, char **argv) {
-	glutInit(&argc, argv);
-	String title("Hello world");
+int main() {
+	String title("Svarog Game Engine");
 	enginecore::initializeWindow(800, 600, title.str);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glEnable(GL_DEPTH_TEST);
-	glutInitWindowSize(800, 600); 
-	glutCreateWindow("Svarog Game Engine");
-	glutDisplayFunc(renderScene);
-	glutKeyboardFunc(test);
-	glutMainLoop();
 	return 0;
 }
--- main.h
+++ main.h
@@ -1,16 +1,58 @@
-#include "EngineHeader.h"
 #include "core/String.h"
+#include "main/Color.h"
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 namespace enginecore {
-
+    Rgb bgcolor(0.0f, 0.0f, 0.0f);
     int width, height = 0;
 
+    void setBGcolor_vals(float r, float g, float b) {
+        bgcolor.setRVal(r);
+        bgcolor.setGVal(g);
+        bgcolor.setBVal(b);
+    }
+    
+    void framebuffersizecallback(GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    void processInput(GLFWwindow* window) {
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
+
     void initializeWindow(int w, int h, String title) {
+        #ifdef __APPLE__ 
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        #endif
         width = w;
         height = h;
-        glutInitDisplayMode(GLUT_DEPTH | GLUT_RGBA | GLUT_DOUBLE);
-        glEnable(GL_DEPTH_TEST);
-        glutInitWindowSize(width, height);
-        glutCreateWindow(title.str);
-        glutMainLoop();
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        
+        GLFWwindow* window = glfwCreateWindow(width, height, title.str, NULL, NULL);
+        if(window == NULL) {
+            glfwTerminate();
+        }
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffersizecallback);
+
+        if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+            cout << "glad not processed" << endl;
+            glfwTerminate();
+        }
+
+        while(!glfwWindowShouldClose(window)) {
+            processInput(window);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            Rgb test(255.0f, 0.0f, 0.0f);
+            glClearColor(test.redval/255, test.greenval, test.blueval, 0.0f);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        glfwTerminate();
     }
 }
