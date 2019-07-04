711fcd290b0c3634d8df7516b2b9b866faf6f9ca
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-07-04 17:43:31 -0400

    Add files via upload

---
+++ main/shaders/FragmentShader.glsl
@@ -0,0 +1,5 @@
+#version 400
+out vec4 frag_colour;
+void main() {
+    frag_colour = vec4(0.5, 1.0, 0.2, 0.0);
+};
--- main/shaders/material.h
+++ main/shaders/material.h
@@ -3,39 +3,106 @@
 #define MATERIAL_H
 #endif
 #include<iostream>
+#include<sstream>
 #include<fstream>
-#include "color/Color.h"
+#include<string>
+#include<glad/glad.h>
 #include "core/String.h"
 #include "core/ArrayList.h"
-#include "math/vec2.h"
-#include "math/vec3.h"
 #include "math/vec4.h"
 using namespace std;
 
 class Material {
-    private:
-        String versionNum;
-        String locationVar;
     public:
-        unsigned int ID;
-        Material(){}
-        String inputVarLoc;
-        String outputVar;
-        String attribpos;
-        int shaderType;
-        Material(String inputVarLoc, String outputVar, String attribpos, int shaderType) {
-            this->inputVarLoc = inputVarLoc;
-            this->outputVar = outputVar;
-            this->attribpos = attribpos;
-            this->shaderType = shaderType;
-            versionNum.str = "#version 330 core";
-            if(shaderType == -1) {
-                ID = 1; // means a vertex shader
-            } else if(shaderType == 1) {
-                ID = 2; // means fragment shader
+        unsigned int shaderID;
+        Material(String vertexShaderPath, String fragmentShaderPath) {
+            // shader info
+            string vertshader;
+            string fragshader;
+            ifstream vertexShader;
+            ifstream fragmentShader;
+            const char* vertShadercode;
+            const char* fragShaderCode;
+            vertexShader.exceptions(ifstream::failbit);
+            fragmentShader.exceptions(ifstream::failbit);
+
+            // reads file for shader info
+            try {
+                vertexShader.open(vertexShaderPath.str);
+                fragmentShader.open(fragmentShaderPath.str);
+                std::stringstream vertShaderStream, fragShaderStream;
+                vertShaderStream << vertexShader.rdbuf();
+                fragShaderStream << fragmentShader.rdbuf();
+                vertexShader.close();
+                fragmentShader.close();
+                vertshader = vertShaderStream.str();
+                fragshader = fragShaderStream.str();
+                // file info read into str
+            } catch (ifstream::failure e) {
+                cout << "SHADER FAILURE TO COMPILE" << endl;
+            }
+            const char* vShaderCode = vertshader.c_str();
+            const char* fShaderCode = fragshader.c_str();
+
+            // vertex shader compile
+            unsigned int vertexshader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexshader, 1, &vShaderCode, nullptr);
+            glCompileShader(vertexshader);
+            checkcomperrors(vertexshader, "VertexShader");
+
+            // fragment shader compile
+            unsigned int fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentshader, 1, &fShaderCode, NULL);
+            glCompileShader(fragmentshader);
+            checkcomperrors(fragmentshader, "FragmentShader");
+
+            // shader program
+            shaderID = glCreateProgram();
+            glAttachShader(shaderID, vertexshader);
+            glAttachShader(shaderID, fragmentshader);
+            glLinkProgram(shaderID);
+            checkcomperrors(shaderID, "Main");
+
+            // deletes shaders from memory
+            glDeleteShader(vertexshader);
+            glDeleteShader(fragmentshader);
+
+        }
+        void use() {
+            glUseProgram(shaderID);
+        }
+
+        void setBool(const string &name, bool val) const {
+            glUniform1i(glGetUniformLocation(shaderID, name.c_str()), (int)val);
+        }
+
+        void setInt(const string &name, int val) const {
+            glUniform1i(glGetUniformLocation(shaderID, name.c_str()), val);
+        }
+
+        void setFloat(const string &name, float val) const {
+            glUniform1i(glGetUniformLocation(shaderID, name.c_str()), val);
+        }
+
+    private:
+        // shader compile check
+        void checkcomperrors(unsigned int fragment, string type) {
+            int success;
+            char info[1024];
+            if(type != "Main") {
+                glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+                if(!success) {
+                    glGetShaderInfoLog(fragment, 1024, NULL, info);
+                    cout << "Shader type: " << type << "had an error on compile" << endl;
+                    cout << "Error info: " << info << endl;
+                } 
+            } else {
+                glGetShaderiv(fragment, GL_LINK_STATUS, &success);
+                if(!success) {
+                    glGetProgramInfoLog(fragment, 1024, NULL, info);
+                    cout << "Error! Shader program failed to compile" << endl;
+                    cout << "Error info: " << info[0] << endl;
+                }
             }
         }
-        template<typename T>
-        ArrayList<T>createShader();
-        void writeShader(ArrayList<String>file, String filepath, String filename);
 };
