f1ee421b1509239081d017396c83dd115bc232f6
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-07-12 18:21:51 -0400

    Add files via upload

--- main.h
+++ main.h
@@ -1,19 +1,10 @@
 #include "core/String.h"
 #include "main/color/Color.h"
+#include "main/shaders/material.h"
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 namespace enginecore {
     int width, height = 0;
-    const char *shader="#version 400\n"
-    "in vec3 vp;"
-    "void main() {"
-    "  gl_Position = vec4(vp, 1.0);"
-    "}";
-    const char *fragmentShaderSource = "#version 400\n"
-    "out vec4 frag_colour;"
-    "void main() {"
-    "  frag_colour = vec4(0.5, 0.2, 0.2, 0.0);"
-    "}";
     void framebuffersizecallback(GLFWwindow* window, int width, int height) {
         glViewport(0, 0, width, height);
     }
@@ -45,69 +36,34 @@ namespace enginecore {
         }
         glfwMakeContextCurrent(window);
         glfwSetFramebufferSizeCallback(window, framebuffersizecallback);
-
         if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
             cout << "glad not processed" << endl;
             glfwTerminate();
         }
 
-        // compiling of shader
-        GLuint VertShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(VertShader, 1, &shader, NULL);
-        glCompileShader(VertShader);
-
-        // shader success
-        int success;
-        char infolog[512];
-        glGetShaderiv(VertShader, GL_COMPILE_STATUS, &success);
-        if(!success) {
-            glGetShaderInfoLog(VertShader, 512, NULL, infolog);
-            cout << "SHADER FAILED, '\n'INFO ON FAILURE:'\n'" << infolog << endl;
-        }
-        GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragShader, 1, &fragmentShaderSource, NULL);
-        glCompileShader(fragShader);
-        glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
-        if(!success) {
-            glGetShaderInfoLog(fragShader, 512, NULL, infolog);
-            cout << "SHADER FAILED, '\n'INFO ON FAILURE:'\n'" << infolog << endl;
-        }
-        // compiling of shader program
-        GLuint shaderProgram = glCreateProgram();
-
-        // attaching of shaders
-        glAttachShader(shaderProgram, VertShader);
-        glAttachShader(shaderProgram, fragShader);
-        glLinkProgram(shaderProgram);
-        glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
-        if(!success) {
-            glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
-            cout << "Shader program compile failed. '\n' info on crash:'\n'" << infolog << endl;
-        }
-
-        // making of shape
+        String vertexpath("/home/jordan/Documents/SvarogGameEngine/main/shaders/VertexShader.glsl");
+        String fragmentpath("/home/jordan/Documents/SvarogGameEngine/main/shaders/FragmentShader.glsl");
+        Material test3(vertexpath.str, fragmentpath.str);
         float verticies[] {
-            -0.5, -0.5, 0.0,
-            0.5, -0.5, 0.0,
-            0.0, 0.5, 0.0,
+            -0.5, -0.5, 
+            0.5, -0.5, 
+            0.0, 0.5,
         };
         unsigned int VBO, VAO;
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
-
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2* sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
         while(!glfwWindowShouldClose(window)) {
             processInput(window);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            Rgb test(255.0f, 0.0f, 0.0f);
-            glClearColor(test.redval/255, test.greenval, test.blueval, 0.0f);
-            glUseProgram(shaderProgram);
+            glClearColor(1.0, 0.0, 1.0, 1.0);
+            test3.use();
             glBindVertexArray(VAO);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             glfwSwapBuffers(window);
