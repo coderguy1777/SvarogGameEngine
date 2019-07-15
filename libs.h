1bc8b1bd98fac214ae44a3d18f1177f5e2cde7ce
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-07-15 16:47:24 -0400

    Add files via upload

---
+++ libs.h
@@ -0,0 +1,10 @@
+#include "core/String.h"
+#include "main/keyboard.h"
+#include "main/color/Color.h"
+#include "main/shaders/material.h"
+#include "core/Point.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
