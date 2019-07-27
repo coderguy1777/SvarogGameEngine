cca21f86a1d922c14ad7b981d8a1c5f1744787c6
coderguy1777 <hill.jordan@charterschool.org>
2019-07-27 10:16:34 -0400

    Camera matrix work continued, eye plane done.

--- math/camera_matrix.cpp
+++ math/camera_matrix.cpp
@@ -1,5 +1,8 @@
 #include "camera_matrix.h"
-
 void CameraMatrix::find_ClipSpace(Vector4 clipVec) {
     out_clipVec(clipVec);
+}
+
+Vector3 CameraMatrix::out_clipVec(Vector4 clipVec)  {
+    return Vector3(clipVec.x/clipVec.w, clipVec.y/clipVec.w, clipVec.z/clipVec.w);
 }
--- math/camera_matrix.h
+++ math/camera_matrix.h
@@ -10,6 +10,9 @@ class CameraMatrix : public Matrix4f {
         // 6 view cooordinates for projection matrix
         float left, right, bottom, top, near, far;
 
+        // clip vec coords.
+        float x_c, y_c, z_c, w_c;
+
         // matricie dimensions
         const int matrow = 4;
         const int matcol = 4;
@@ -23,13 +26,56 @@ class CameraMatrix : public Matrix4f {
             }
         }
 
+        CameraMatrix(float l, float r, float b, float t, float n, float f) {
+            this->left = l;
+            this->right = r;
+            this->bottom = b;
+            this->top = t;
+            this->near = n;
+            this->far = f;
+        }
+
+        void * operator new(size_t size) {
+            void * p = new ::CameraMatrix();
+            return p;
+        }
+
         // function meant to find the clip space
         // coordinates or a class
         static void find_ClipSpace(Vector4 clipVec);
 
         // finds clip_vec for 3d space inline statically for performance.
-        static inline Vector3 out_clipVec(Vector4 clipVec) {
-            return Vector3(clipVec.x/clipVec.w, clipVec.y/clipVec.w, clipVec.z/clipVec.w);
+        static inline Vector3 out_clipVec(Vector4 clipVec);
+
+        // checks if vertex is negative, if negative
+        // gets discarded.
+        inline bool discard_Vertex(Vector3 lipVec) {
+            return (lipVec.initx < -w_c || lipVec.initx > w_c || lipVec.inity < -w_c || lipVec.inity > w_c || lipVec.initz < -w_c || lipVec.initz > w_c) ? true : false;
         }
+
+        // eye plane function
+        void compute_eye_plane();
+
+        // eye plane points
+        inline void findX_EYE();
+        inline void findY_EYE();
+        inline void findZ_EYE();
+
+        // triggers projection_plane computation.
+        void compute_project_plane() {
+            findX_P();
+            findY_P();
+            findZ_P();
+        }
+
+        // finds projection plane values
+        inline void findX_P();
+        inline void findY_P();
+        inline void findZ_P();
+
+
+        private:
+            float x_p, y_p, z_p;
+            float x_e, y_e, z_e;
         
 };
