0a8997954663671740c467b1c0fb9e5a3e763d95
coderguy1777 <hill.jordan@charterschool.org>
2019-08-15 12:21:10 -0400

    Quick keyboard class function change to remove errors from functions I plan to rewrite later, tommorow that as my TODO for that day.

--- core/Input/keyboard.cpp
+++ core/Input/keyboard.cpp
@@ -1,10 +1,6 @@
 #include "keyboard.h"
 bool Keyboard::isKeyPressed(GLFWwindow* window, int keystate) {
-    return (glfwGetKey(window, keystate) == GLFW_PRESS) ? true : false;
-}
-
-bool Keyboard::isKeyReleased(int keystate) {
-    return (glfwGetKey(keywin, keystate) == GLFW_RELEASE) ? true : false;
+    //return (glfwGetKey(window, keystate) == GLFW_PRESS) ? true : false;
 }
 
 bool Keyboard::isKeyRepeat(int keystate) {
@@ -13,18 +9,6 @@ bool Keyboard::isKeyRepeat(int keystate) {
 
 int Keyboard::getKeyCode(int key) {
     return key;
-}
-
-bool Keyboard::checkKeyPressed(int keycode) {
-    return (glfwGetKey(winNN.initliazeWindow(), keycode) == GLFW_PRESS) ? true : false;
-}
-
-bool Keyboard::checkKeyReleased(int keycode) {
-    return (glfwGetKey(winNN.initliazeWindow(), keycode) == GLFW_RELEASE) ? true : false;
-}
-
-bool Keyboard::checkKeyRepeat(int keycode) {
-    return (glfwGetKey(winNN.initliazeWindow(), keycode) == GLFW_REPEAT) ? true : false;
 }
 
 bool Keyboard::keyPress(GLFWwindow* window) {
