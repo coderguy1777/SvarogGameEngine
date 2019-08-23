405d1eb01d69fa41465480fc0329296ce3c46b0a
coderguy1777 <hill.jordan@charterschool.org>
2019-08-23 12:51:37 -0400

    Libraries script now being made, python wont be used to do the install process in this case. For windows however that may be needed for it to run, TBD later on in development.

---
+++ install_libraries.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+LIB_GLFW="glfw3"
+LIB_X11="X11"
+LIB_BLD_ESSENTIAL="build-essential"
+LIB_XINERMA="Xinerma"
+LIB_GLU="GLU"
+LIB_DL="dl"
+LIB_XI="Xi"
+LIB_XRANDR="Xrandr"
+LIB_ASSIMP="assimp"
+LIB_XCURSOR="Xcursor"
+
+echo "Beginning Library install, press [Y] to continue, or [N] to stop the library installs."
