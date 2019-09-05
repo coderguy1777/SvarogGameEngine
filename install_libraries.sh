bce74846e98e44c415d4fc52f6b4fa3890a55cab
coderguy1777 <hill.jordan@charterschool.org>
2019-09-04 23:40:12 -0400

    Library installing script working finally for the shell, may need to add cmake scripts for finding GLFW3 on cross plat machines however.

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,20 +1,71 @@
 #!/bin/bash
-LIB_GLFW="glfw3"
-LIB_X11="X11"
+# library name variables
+LIB_GLFW="libglfw3-dev"
+LIB_X11="libx11-dev"
 LIB_BLD_ESSENTIAL="build-essential"
-LIB_XINERMA="Xinerma"
-LIB_GLU="GLU"
-LIB_DL="dl"
-LIB_XI="Xi"
-LIB_XRANDR="Xrandr"
-LIB_ASSIMP="assimp"
-LIB_XCURSOR="Xcursor"
+LIB_XINERMA="x11proto-xinerama-dev"
+LIB_GLU="libglu1-mesa-dev"
+LIB_DL="libdl-dev"
+LIB_XI="libxi-dev"
+LIB_XRANDR="libxrandr-dev"
+LIB_ASSIMP="libassimp-dev"
+LIB_XCURSOR="libxcursor-dev"
 
-echo "Beginning Library install, press [Y] to continue, or [N] to stop the library installs."
-read ans
+# running of checks for libraries.
+GLFW_CHECK=$(dpkg -s libglfw3-dev|grep installed)
+echo Checking for glfw3: $GLFW_CHECK
+if [ "" == "$GLFW_CHECK" ]; then
+    echo "Setting up glfw3 now..."
+    cd && mkdir glfw_dir
+    cd glfw_dir && git clone https://github.com/glfw/glfw
+    cd glfw && cmake .
+    cmake -DBUILD_SHARED_LIBS=ON .
+elif [ "" != "$GLFW_CHECK" ]; then
+    echo "GLFW is installed, continuing lib check."
+fi
 
-if [$ans == 'Y']; then 
-    echo 'Continuing.....'
-elif [$ans == 'N']; then
-    echo "Ending now......"
+X11_CHECK=$(dpkg -s libx11-dev|grep installed)
+echo Checking for X11: $X11_Check
+if [ "" == "$X11_CHECK" ]; then 
+    echo "Setting up X11 now...."
+    sudo apt install libx11-dev && sudo apt update
+elif [ "" != "$X11_CHECK" ]; then 
+    echo "X11 is installed, continuing lib check."
+fi
+
+BLD_ESNT=$(dpkg -s build-essential|grep installed)
+echo Checking for build-essential: $BLD_ESNT
+if [ "" == "$BLD_ESNT" ]; then
+    echo "Setting up build essential now..."
+    sudo apt-get install build-essential
+elif [ "" != "$BLD_ESNT" ]; then
+    echo "Build Essential is installed, continuing lib check."
+fi
+
+XINERMA_CHECK=$(dpkg -s x11proto-xinerama-dev|grep installed)
+echo Checking for Xinerma: $XINERMA_CHECK
+if [ "" == "$XINERMA_CHECK" ]; then
+    echo "Setting up Xinerma now..."
+    sudo apt-get install x11proto-xinerama-dev
+elif [ "" != "$XINERMA_CHECK" ]; then
+    echo "Xinerma installed, continuing lib check."
+fi
+
+GLU_CHECK=$(dpkg -s libglu1-mesa-dev|grep installed)
+echo Checking for GLU: $GLU_CHECK
+if [ "" == "$GLU_CHECK" ]; then
+    echo "Setting up GLU now..."
+    sudo apt-get install libglu1-mesa-dev
+elif [ "" != "$XINERMA_CHECK" ]; then
+    echo "GLU installed, continuing lib check."
+fi
+
+# TODO: check if dl needs to be installed
+DL_CHECK=$(dpkg -s libdl-dev|grep installed)
+echo Checking for DL: $DL_CHECK
+if [ "" == "$DL_CHECK" ]; then
+    echo "Setting up DL now..."
+    sudo apt-get install dl
+elif [ "" != "$DL_CHECK" ]; then
+    echo "dl installed, continuing lib check."
 fi
