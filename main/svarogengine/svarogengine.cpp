25800ff9109edeac9ae4cc3024c939bfe3928d83
coderguy1777 <hill.jordan@charterschool.org>
2019-09-30 11:00:27 -0400

    Added imgui

--- main/svarogengine/svarogengine.cpp
+++ main/svarogengine/svarogengine.cpp
@@ -1,5 +1,12 @@
-#include "svarogengine.h"
+#include "libraries/imgui/imgui.h"
+#include "libraries/imgui/examples/imgui_impl_glfw.h"
+#include "libraries/imgui/examples/imgui_impl_opengl3.h"
 #define Vector3 vec3
+#include "svarogengine.h"
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include<glad/glad.h>
+#endif
+
 SvarogEngine* SvarogEngine::getInstanceEngine() {
     if(!engine_instance) engine_instance = new SvarogEngine; return engine_instance;
 }
@@ -33,24 +40,6 @@ void SvarogEngine::RunEngine() {
         "{\n"
         "   frag_color = vec4(sin(abs(time)), float_tst.y, float_tst.z, 1.0);\n"
         "}\n\0";
-    
-    ShaderGeneration * ab = new ShaderGeneration("shader_frag.glsl", 400, 1, true);
-    ArrayList<const char*>xss;
-    ab->add_to_output("#version 400\n");
-    ab->add_to_output("layout (location = 0) in vec3 aPos;\n");
-    ab->add_to_output("out frag_color;\n");
-    ab->add_to_output("uniform vec4 float_tst;\n");
-    ab->add_to_output("uniform float time;\n");
-    ab->add_to_output("void main()\n");
-    ab->add_to_output("{\n");
-    ab->add_to_output("   frag_color=vec4(sin(abs(time)), float_tst.y, float_tst.z, 1.0);\n");
-    ab->add_to_output("}\n\0");
-    ab->write_file();
-    if(ab->get_success() == 1) {
-        spdlog::info("Shader written.");
-    } else if(ab->get_success() != 1) {
-        spdlog::info("WRITE_FAIL");
-    }
 
     VertexShader vert_mat;
     vert_mat.init_state(2);
@@ -101,16 +90,37 @@ void SvarogEngine::RunEngine() {
     ArrayList<RenderObj*>x;
     x.add(s);
     test_1->use();
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(static_cast<GLFWwindow*>(EngineWindow::getInstance()->getWindow()), true);
+    ImGui_ImplOpenGL3_Init("#version 400");
+    bool show_win = true;
     while(EngineWindow::getInstance()->get_state()) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(1.0, 0.0, 0.0, 1.0);
         float xx = glfwGetTime();
         float y = sin(xx / 2.0f) * 500.0f;
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::Begin("Demo");
+        if(ImGui::Button("Hello")) {
+            spdlog::warn("Button pressed");
+        }
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glUniform4f(glGetUniformLocation(test_1->get_shader_id(), "float_tst"), 0.0f, y, 0.0f, 1.0f);
         glUniform1f(glGetUniformLocation(test_1->get_shader_id(), "time"), xx);
         x.get(0)->get_mesh().draw();
         EngineWindow::getInstance()->OnUpdate();
     }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     x.get(0)->get_mesh().del_buffers();
     glDeleteShader(vert_mat.get_shader_id());
     glDeleteShader(frag_mat.get_shader_id());
