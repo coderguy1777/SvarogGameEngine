23daaaabf14cd117e5ffa1d4d7fb73f602605c12
coderguy1777 <hill.jordan@charterschool.org>
2019-10-23 12:38:25 -0400

    fixes with input text boxx

--- main/svarogengine/svarogengine.cpp
+++ main/svarogengine/svarogengine.cpp
@@ -107,6 +107,7 @@ void SvarogEngine::RunEngine() {
             ImGui::SetCursorPos(ImVec2(10, 10));
             SvarogButton a(100, 60, "Debug_Button", true);
             char* buf;
+            ImGui::InputText("##text", buf, IM_ARRAYSIZE(buf));
             unsigned int bc = 20;
             static float size_m = 0.0f;
             ImGui::SliderFloat("Debug_Slider", &size_m, 0.0f, 1.0f);
