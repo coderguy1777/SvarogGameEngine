c740ead4bd1b3fcdc460be98da7a91d145563534
coderguy1777 <hill.jordan@charterschool.org>
2019-10-25 14:03:40 -0400

    Imgui layer created.

--- main/enginewindow/engine-gui/gui_layer.h
+++ main/enginewindow/engine-gui/gui_layer.h
@@ -2,24 +2,45 @@
 #ifndef GUI_LAYER_H
 #define GUI_LAYER_H
 #include "macrodefs.h"
+#include "core/String.h"
+#include "core/ds-classes/ClassString.h"
 #include "core/ds-classes/Stack.h"
-enum class LAYER_ITEM_TYPES {
 
+struct GuiLayerID {
+    unsigned int gui_id;
+    char gui_char_id;
 };
-template <class T>
+
+struct ButtonPosition{
+    float pos_x;
+    float pos_y;
+};
+
+struct LabelPosition {
+    float pos_x;
+    float pos_y;
+};
+
+struct InputTextPosition {
+    float pos_x;
+    float pos_y;
+};
+
 class ImGuiLayer {
     private:
-        Stack<T>*component_stack;
-    public: 
-        ImGuiLayer() {
-            component_stack=new Stack<T>();
-        }
+    struct ButtonStack {
+        GuiLayerID button_id;
+        Stack<bool>gui_window_button;
+        Stack<String>gui_window_button_text;
+    } button_stack;
 
-        void add_gui_component(T const&);
-        T get_gui_component() const;
-        void pop_gui_component();
-        T get_top_component() const;
+    struct LabelStack {
+        GuiLayerID label_id;
+        Stack<std::function<void()>>gui_window_labels;
+        Stack<String>gui_label_text;
+    }label_stack;
+    public: 
 };
 
-
+extern ClassString ENGINE_GUI_LAYER;
 #endif
