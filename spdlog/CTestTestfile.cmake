a2b265ff080ab8b142bd87cfe7f1f00e06433689
Zyansheep <zykersheep@gmail.com>
2019-11-21 17:12:28 -0500

    add glad and glm libs

---
+++ spdlog/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zyansheep/Code/Proj/C/SvarogGameEngine/spdlog
+# Build directory: /home/zyansheep/Code/Proj/C/SvarogGameEngine/spdlog
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("example")
+subdirs("tests")
---
+++ spdlog/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zyansheep/Code/Proj/C/SvarogGameEngine/spdlog/tests
+# Build directory: /home/zyansheep/Code/Proj/C/SvarogGameEngine/spdlog/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(spdlog-utests "/home/zyansheep/Code/Proj/C/SvarogGameEngine/spdlog/tests/spdlog-utests")
