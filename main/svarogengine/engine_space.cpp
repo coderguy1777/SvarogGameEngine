b962f3d2b3b3b65f44c14be43340852571a976cf
coderguy1777 <hill.jordan@charterschool.org>
2020-02-25 12:47:42 -0500

    shit dopne

--- main/svarogengine/engine_space.cpp
+++ main/svarogengine/engine_space.cpp
@@ -7,12 +7,109 @@ EngineSpace* EngineSpace::getEngineSpace() {
 }
 
 void EngineSpace::init_window_settings() {
-    glfw_window_entities.init_window_context();
-    glfw_window_entities.init_win_monitor_context();
+    EngineSpace::glfw_window_entities.init_window_context();
+    EngineSpace::glfw_window_entities.init_win_monitor_context();
 }
 
 void EngineSpace::run() {
-    bool run_state = (!glfwWindowShouldClose && engine_settings.getEngineState() && engine_settings.getState == 1) ? true : false;
+    glfwSetKeyCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, int key, int action, int scancode, int mods) {
+        EngineWindow* key_evt_ptr = static_cast<EngineWindow*>(glfwGetWindowUserPointer(window));
+        switch(scancode) {
+            case GLFW_PRESS:
+                { 
+                    Event e(EVENT_TYPE::KeyEvt, 1, "key_press");
+                    
+                    KeyEvent evt(static_cast<int>(key));
+                    evt.set_key_evt_state(1);
+                    evt.logKeyPressEvent();
+                    
+                }
+
+            case GLFW_REPEAT:
+                {
+                    Event e(EVENT_TYPE::KeyEvt,  2, "key_repeat");
+                    KeyEvent *evt = new KeyEvent(static_cast<int>(key));
+                    evt->set_key_evt_bind(evt->get_keybind());
+                    evt->set_key_evt_event(e);
+                    evt->set_key_evt_state(2);
+                    evt->logKeyHeldEvent();
+                    break;
+                }
+
+            case GLFW_RELEASE:
+                {
+                    Event e(EVENT_TYPE::KeyEvt, 3, "key_release");
+                    KeyEvent evt(static_cast<int>(key));
+                    evt.set_key_evt_state(3);
+                    evt.logKeyReleaseEvent();
+                    break;
+                }
+        }
+    });
+
+    glfwSetMouseButtonCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, int button, int action, int mods) {
+        EngineWindow* mouse_evt_ptr = (EngineWindow*)glfwGetWindowUserPointer(window);
+        switch(action) {
+            // keep this, glfw uses GLFW_PRESS
+            case GLFW_PRESS:
+                {
+                    Event e(EVENT_TYPE::MouseEvt, 1, "mouse_press");
+                    MouseEvent evt(static_cast<int>(button), static_cast<unsigned int>(1));
+                    evt.logMousePressEvent();
+                    if(button == SVAROG_MOUSE_BUTTON_MIDDLE) {
+
+                    }
+                    break;
+                }
+            // ---------------------------------------------------------
+            // keep this, glfw uses GLFW_RELEASE
+            case GLFW_RELEASE:
+                {
+                    Event e(EVENT_TYPE::MouseEvt, 3, "mouse_release");
+                    MouseEvent evt(static_cast<int>(button), static_cast<unsigned int>(3));
+                    evt.logMouseReleaseEvent();
+            
+                    break;
+                }
+        }
+    });
+
+    glfwSetScrollCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, double xoffset, double yoffset) {
+        EngineWindow* cursor_scroll_mg = (EngineWindow*)glfwGetWindowUserPointer(window);
+        cursor_scroll_mg->is_back = true;
+    });
+
+    glfwSetWindowCloseCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window) {
+        EngineWindow* win_close_mg = (EngineWindow*)glfwGetWindowUserPointer(window);
+        win_close_mg->reset_bool_state();
+    });
+
+    glfwSetWindowSizeCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, int w, int h) {
+        EngineWindow* win_size_mg = (EngineWindow*)glfwGetWindowUserPointer(window);
+        glViewport(0, 0, w, h);
+        win_size_mg->winA.changeHeight(h);
+        win_size_mg->winA.changeWidth(w);
+        spdlog::info("WINDOW_RESIZE");
+    });
+
+    glfwSetCharCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, unsigned int keycode) {
+        EngineWindow* key_typed_mg = (EngineWindow*)glfwGetWindowUserPointer(window);
+    });
+
+    // TODO: fix rendering bug with frame buffer callback.
+    glfwSetFramebufferSizeCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, int w, int h) {
+        EngineWindow* frm_bfer_cb = (EngineWindow*)glfwGetWindowUserPointer(window);        
+        glfwGetFramebufferSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+        spdlog::info("FRAMEBUFFER_SIZE_CHANGE");
+    });
+
+    glfwSetWindowPosCallback(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()), [](GLFWwindow* window, int xpos, int ypos) {
+        EngineWindow* scle_bfer_cb = (EngineWindow*)glfwGetWindowUserPointer(window);
+    });
+
+
+    bool run_state = (!glfwWindowShouldClose && engine_settings.getEngineState() && engine_settings.getState() == 1) ? true : false;
     while(run_state) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_LIGHTING | GL_COLOR_MATERIAL);
--- main/svarogengine/engine_space.h
+++ main/svarogengine/engine_space.h
@@ -37,36 +37,31 @@ class EngineSpace {
     private: 
         static struct EngineWindowStyle {
             private: 
-                std::string window_name;
-                uint w;
-                uint h;
+                std::string window_name = "Svarog Engine";
+                uint w = 1920;
+                uint h = 1080;
 
             public:
-                inline void set_window_width(const uint& width) { w = width; }
-                inline void set_window_height(const uint& height) { h = height; }
-                inline void set_window_name(const std::string&win_name) { window_name = win_name; }
                 uint get_window_width() const { return w; }
                 uint get_window_height() const { return h; }
                 std::string get_window_name() const { return window_name; }
 
-        } engine_style_settings;
+        };
 
         // settings for the window relating to vsync, etc.
         static struct EngineWindowSettings {
             private: 
-                bool isVsyncOn;
-                uint state;
-                bool engine_state;
+                bool isVsyncOn = true;
+                uint state = 1;
+                bool engine_state = (state == 1) ? true  : false;
 
             public: 
-                inline void setVsyncOn(uint state) { isVsyncOn = (state == 1) ? true : false; }
-                inline void setState(bool run) { state = (run == true) ? 1 : 0; }
-                inline void setEngineState(uint state) { engine_state = (state == 1) ? true : false; } 
-                bool getVsyncState() const { return isVsyncOn; }
-                bool getEngineState() const { return engine_state; }
-                uint getState() const { return state; }
+                static void state_update(uint new_state) { state = new_state; }
+                static bool getVsyncState() const { return isVsyncOn; }
+                static bool getEngineState() const { return engine_state; }
+                static uint getState() const { return state; }
 
-        } engine_settings;
+        };
 
         static struct GLFWEntities {
             private: 
@@ -74,9 +69,9 @@ class EngineSpace {
                 GLFWwindow* engine_glfw_win;
                 std::shared_ptr<WindowMonitor>win_mon = std::make_shared<WindowMonitor>();
             public: 
-                inline void* getEngineWin() { return engine_glfw_win; }
-                inline void* getEngineMonitor() { return engine_glfw_mon; }
-                inline void make_context_curr() { 
+                 void* getEngineWin() { return engine_glfw_win; }
+                 void* getEngineMonitor() { return engine_glfw_mon; }
+                void make_context_curr() { 
                     WindowContext::make_curr_context(static_cast<GLFWwindow*>(getEngineWin())); 
                 }
 
@@ -88,11 +83,11 @@ class EngineSpace {
                     WindowContext::set_states(1, 3);
                     WindowContext::init_glfw();
                     WindowContext::create_context(static_cast<GLFWwindow*>(getEngineWin()));
-                    engine_glfw_win = glfwCreateWindow((int)engine_style_settings.get_window_width(), (int)engine_style_settings.get_window_height(), (const char*)engine_style_settings.get_window_name().c_str(), NULL, NULL);
+                    engine_glfw_win = glfwCreateWindow((int)EngineWindowStyle::get_window_width(), (int)EngineWindowStyle::get_window_height(), (const char*)engine_style_settings.get_window_name().c_str(), NULL, NULL);
                 }
 
-                void runVSYNC() {
-                    auto check_VSYNC = EngineSpace::engine_settings.getVsyncState();
+                inline void runVSYNC() {
+                    auto check_VSYNC = engine_settings.getVsyncState();
                     if(check_VSYNC) {
                         glfwSwapInterval(1);
                     } else if(!check_VSYNC) {
@@ -100,18 +95,26 @@ class EngineSpace {
                     }
                 }
                 
-        } glfw_window_entities;
+        };
         static EngineSpace* engine;
-        EngineSpace() {}
+        EngineSpace() {
+            init_imgui_context();
+            init_window_settings();
+        }
     public: 
         static EngineSpace* getEngineSpace();
+        inline void init_imgui_context() {
+            ImGuiInit::make_imgui_context(static_cast<GLFWwindow*>(GLFWEntites::getEngineWin()), "#version 400");
+            ImGuiInit::make_imgui_style(0);
+            ImGuiInit::imgui_ini_use(false);
+        }
         inline void init_window_settings();
         inline void run();
         inline void pre_batch();
         inline void load_gpu_data();
         
         ~EngineSpace() {
-            glfwDestroyWindow(static_cast<GLFWwindow*>(glfw_window_entities.getEngineWin()));
+            glfwDestroyWindow(static_cast<GLFWwindow*>(GLFW.getEngineWin()));
         }
 
 };
